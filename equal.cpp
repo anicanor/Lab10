f94cb462e3be271ba096d7a2254ce492d2c8aba1
anicanor <31227133+anicanor@users.noreply.github.com>
2018-03-31 23:00:15 -0700

    Create equal.cpp

---
+++ equal.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+bool equal_string(const char str1[], const char str2[]){
+    
+    if(str1[0] == 0 && str2[0] == 0) return true;
+
+    if(str1[0] != str2[0]) return false;
+    else return equal_string(str1+1, str2+1);
+}
+
+int main(){
+    
+    char str1[1000], str2[1000];
+
+    str1[0] = 0;
+    str2[0] = 0;
+
+    cout << "enter first string" << endl;
+    cin >> str1;
+    cout << "enter second string" << endl;
+    cin >> str2;
+
+    if (equal_string(str1, str2))
+        {cout << str1 << " is the same as " << str2 << endl;}
+    else 
+        {cout << str1 << " is not the same as " << str2 << endl;}
+}
