e0b79f3f3da702d0ff6aeb1c4e2b8999845f2ff4
anicanor <31227133+anicanor@users.noreply.github.com>
2018-03-31 22:48:27 -0700

    Create isint.cpp

---
+++ isint.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <assert.h>
+#include <ctype.h>
+
+using namespace std;
+
+bool is_int(char str[]){ 
+    
+    if(!isdigit(str[0])||str[0]==0) return false;
+    else if(str[1]==0) return true;
+
+    return is_int(str+1);
+}
+
+int main(){
+    
+  char check[1000];
+  cout << "Enter a string." << endl;
+  cin >> check;
+
+  if (is_int(check))
+    {cout << check << " is an integer." << endl;}
+  else 
+    {cout << check << " is not an integer." << endl;}
+}
